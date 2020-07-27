d645677ca9e8e7b8328a76abd48b35f49b4dc5fa
João victor <santosjoao133@gmail.com>
2020-07-27 12:45:31 -0300

    setting for project

---
+++ src/components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/Logo.png';
+
+function Menu(){
+	return (
+		<nav>
+			<img src={Logo} alt="NerdolaFlix Logo"/>
+		</nav>
+	);
+}
+
+export default Menu;
